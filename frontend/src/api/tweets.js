d9f0dd161b7e43fe087dd849c05ecde3aea4e530
Laura Augusta <laurah.algusta@gmail.com>
2024-04-29 19:24:52 -0300

    feat-tweets-update

--- frontend/src/api/tweets.js
+++ frontend/src/api/tweets.js
@@ -1,5 +1,20 @@
 import { authAxios } from "./useAxios";
 
+export const deleteTweet = async (id) => {
+    await authAxios.delete(`/tweets/${id}/`);
+}
+
+export const editTweet = async (data) => {
+    await authAxios.put(`/tweets/${data.get('id')}/`, data);
+}
+
+export const getUserTweets = async (username) => {
+    const response = await authAxios.get(`/tweets/my/${username}/`);
+    // console.log(response.data)
+    return response.data;
+
+}
+
 export const getTweets = async ({ pageParam = 1 }) => {
     const response = await authAxios.get(`/tweets/?page=${pageParam}&pages=10`);
     return response.data;
--- tweets/urls.py
+++ tweets/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.TweetList.as_view()),
     path('<int:pk>/', views.TweetDetail.as_view()),
+    path('my/<str:username>/', views.get_user_tweets),
 ]
